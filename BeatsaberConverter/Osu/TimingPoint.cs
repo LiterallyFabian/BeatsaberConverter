daf228ebc942016578001a3370b5d0b379c600b8
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-12 15:00:16 +0100

    Add TimingPoint

---
+++ BeatsaberConverter/Osu/TimingPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatsaberConverter.Osu
+{
+    internal class TimingPoint
+    {
+        public int Time { get; set; }
+
+        public double BeatLength { get; set; }
+
+        public int Meter { get; set; }
+
+        public SampleSet SampleSet { get; set; }
+
+        public int SampleIndex { get; set; }
+
+        public int Volume { get; set; }
+
+        public bool Uninherited { get; set; }
+
+        public bool Kiai { get; set; }
+
+        public TimingPoint(int time, double beatLength, int meter, SampleSet sampleSet, int sampleIndex, int volume, bool uninherited, bool kiai)
+        {
+            Time = time;
+            BeatLength = beatLength;
+            Meter = meter;
+            SampleSet = sampleSet;
+            SampleIndex = sampleIndex;
+            Volume = volume;
+            Uninherited = uninherited;
+            Kiai = kiai;
+        }
+    }
+}
