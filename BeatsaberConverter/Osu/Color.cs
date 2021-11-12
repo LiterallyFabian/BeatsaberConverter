f5b582486a06faac515b856684f09273f1ac4781
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-12 14:57:01 +0100

    Add Color

---
+++ BeatsaberConverter/Osu/Color.cs
@@ -0,0 +1,14 @@
+﻿namespace BeatsaberConverter.Osu
+{
+    internal class Color
+    {
+        public byte r, g, b;
+
+        public Color(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+}
