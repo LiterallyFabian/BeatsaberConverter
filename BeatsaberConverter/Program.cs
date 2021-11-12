21f4b1f7438c0e3456231f42b1fe94c823647746
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-12 13:51:09 +0100

    Remove old comment

--- BeatsaberConverter/Program.cs
+++ BeatsaberConverter/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace BeatsaberConverter // Note: actual namespace depends on the project name.
+namespace BeatsaberConverter
 {
     public class Program
     {
