ba53462aa3648ebe501732cbe87eb711279458dd
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-12 15:02:41 +0100

    Add Beatmap structure

---
+++ BeatsaberConverter/Osu/Beatmap.cs
@@ -0,0 +1,136 @@
+﻿namespace BeatsaberConverter.Osu
+{
+    #region Enums
+
+    public enum SampleSet
+    {
+        None,
+        Normal,
+        Soft,
+        Drum
+    }
+
+    public enum Countdown
+    {
+        NoCountdown = 0,
+        Normal = 1,
+        Half = 2,
+        Double = 3
+    }
+
+    public enum Mode
+    {
+        Osu = 0,
+        Taiko = 1,
+        Catch = 2,
+        Mania = 3
+    }
+
+    public enum OverlayPosition
+    {
+        NoChange,
+        Below,
+        Above
+    }
+
+    #endregion Enums
+
+    internal class Beatmap
+    {
+        #region General
+
+        public string AudioFilename { get; set; } = "audio.mp3";
+
+        public int AudioLeadIn { get; set; } = 0;
+
+        public int PreviewTime { get; set; } = -1;
+
+        public Countdown Countdown { get; set; } = Countdown.Normal;
+
+        public SampleSet SampleSet { get; set; } = SampleSet.Normal;
+
+        public double StackLeniency { get; set; } = 0.7;
+
+        public Mode Mode { get; set; } = Mode.Osu;
+
+        public bool LetterboxInBreaks { get; set; } = false;
+
+        public bool UseSkinSprites { get; set; } = false;
+
+        public OverlayPosition OverlayPosition { get; set; } = OverlayPosition.NoChange;
+
+        public string SkinPreference { get; set; }
+
+        public bool EpilepsyWarning { get; set; } = false;
+
+        public int CountdownOffset { get; set; } = 0;
+
+        public bool SpecialStyle { get; set; } = false;
+
+        public bool WidescreenStoryboard { get; set; } = false;
+
+        public bool SamplesMatchPlaybackRate { get; set; } = false;
+
+        #endregion General
+
+        #region Editor
+
+        public int[] Bookmarks { get; set; }
+
+        public double DistanceSpacing { get; set; }
+
+        public int BeatDivisor { get; set; }
+
+        public int GridSize { get; set; }
+
+        public double TimelineZoom { get; set; }
+
+        #endregion Editor
+
+        #region Metadata
+
+        public string Title { get; set; }
+
+        public string TitleUnicode { get; set; }
+
+        public string Artist { get; set; }
+
+        public string ArtistUnicode { get; set; }
+
+        public string Creator { get; set; }
+
+        public string Version { get; set; }
+
+        public string Source { get; set; }
+
+        public string[] Tags { get; set; }
+
+        public int BeatmapID { get; set; }
+
+        public int BeatmapSetID { get; set; }
+
+        #endregion Metadata
+
+        #region Difficulty
+
+        public double HPDrainRate { get; set; }
+
+        public double CircleSize { get; set; }
+
+        public double OverallDifficulty { get; set; }
+
+        public double ApproachRate { get; set; }
+
+        public double SliderMultiplier { get; set; }
+
+        public double SliderTickRate { get; set; }
+
+        #endregion Difficulty
+
+        public List<TimingPoint> TimingPoints { get; set; } = new List<TimingPoint>();
+
+        public List<Color> Colors { get; set; } = new List<Color>();
+
+        public List<HitObject> HitObjects { get; set; } = new List<HitObject>();
+    }
+}
