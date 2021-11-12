7173c91acfff503214466c1895084d74bec92474
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-12 22:16:06 +0100

    Add HitObject

---
+++ BeatsaberConverter/Osu/HitObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace BeatsaberConverter.Osu
+{
+    internal class HitObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2 Position
+        {
+            get { return new Vector2(X, Y); }
+            set { X = (int)value.X; Y = (int)value.Y; }
+        }
+
+        public int Time { get; set; }
+
+        public int HitSound { get; set; }
+
+        public bool NewCombo { get; set; }
+    }
+}
