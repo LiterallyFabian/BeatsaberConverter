a4c95826cb60f1a9db80275186d07a15b5e5b97b
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-14 14:10:44 +0100

    Add missing line breaks

--- BeatsaberConverter/BeatSaber/Difficulty.cs
+++ BeatsaberConverter/BeatSaber/Difficulty.cs
@@ -3,7 +3,9 @@
     internal class Difficulty
     {
         public string _version { get; set; } = "2.0.0";
+
         public Note[] _notes { get; set; } = Array.Empty<Note>();
+
         public Obstacle[] _obstacles { get; set; } = Array.Empty<Obstacle>();
 
         public Event[] _events { get; set; } = Array.Empty<Event>();
