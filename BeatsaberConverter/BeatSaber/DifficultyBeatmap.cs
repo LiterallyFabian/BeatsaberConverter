406ff84890bc7941626b32f614820e8ecc5b1825
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-11-14 23:29:15 +0100

    Add method to convert osu! difficulties to BeatSaber ones

--- BeatsaberConverter/BeatSaber/DifficultyBeatmap.cs
+++ BeatsaberConverter/BeatSaber/DifficultyBeatmap.cs
@@ -31,8 +31,45 @@
 
         public string _beatmapFilename { get; set; }
 
-        public int _noteJumpMovementSpeed { get; set; }
+        public int _noteJumpMovementSpeed { get; set; } = 10;
 
-        public int _noteJumpStartBeatOffset { get; set; }
+        public int _noteJumpStartBeatOffset { get; set; } = 0;
+
+        /// <summary>
+        /// Tries to convert an osu! difficulty name to beatsaber
+        /// Eg. Insane (osu!) -> Expert (BeatSaber).
+        /// </summary>
+        /// <param name="osuDifficulty">The difficulty name in osu!</param>
+        /// <returns></returns>
+        public static Difficulty OsuToBeatSaberDiff(string osuDifficulty)
+        {
+            osuDifficulty = osuDifficulty.ToLower().Trim();
+
+            string[] easy = { "easy", "kantan", "cup" };
+            string[] normal = { "normal", "futsuu", "salad" };
+            string[] hard = { "hard", "muzukashii", "platter" };
+            string[] expert = { "insane", "expert", "oni", "inner", "ura oni", "rain", "overdose" };
+
+            Difficulty closestDiff = Difficulty.ExpertPlus;
+            if (easy.Contains(osuDifficulty))
+            {
+                closestDiff = Difficulty.Easy;
+            }
+            else if (normal.Contains(osuDifficulty))
+            {
+                closestDiff = Difficulty.Normal;
+            }
+            else if (hard.Contains(osuDifficulty))
+            {
+                closestDiff = Difficulty.Hard;
+            }
+            else if (expert.Contains(osuDifficulty))
+            {
+                closestDiff = Difficulty.Expert;
+            }
+
+            Console.WriteLine($"Converted osu! difficulty \"{osuDifficulty}\" to BeatSaber difficulty \"{closestDiff}\".");
+            return closestDiff;
+        }
     }
 }
